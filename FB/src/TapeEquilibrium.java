95bbbec3443bb24001f895a0fd83688ed395e901
Aysar <ak.production@gmail.com>
2014-09-14 21:02:05 -0400

    Tape eqb updated

--- FB/src/TapeEquilibrium.java
+++ FB/src/TapeEquilibrium.java
@@ -9,22 +9,26 @@
 public class TapeEquilibrium {
     public int solution(int[] A) {
         if (A.length == 2)
-            return Math.abs(A[0] - A[1]);
+            return Math.abs(A[0]-A[1]);
 
-        int sum = 1000000;
-        for (int i=0; i<A.length-1; i++) {
-            int s1=0, s2=0;
-            for (int j=0; j<=i; j++) {
-                s1 += A[j];
-            }
-            for (int k=A.length-1; k>i; k--) {
-                s2 += A[k];
-            }
-            s1 = Math.abs(s1-s2);
-            System.out.println(s1);
-            if (s1 < sum)
-                sum = s1;
+        int [] s1 = new int[A.length-1];
+        s1[0] = A[0];
+        for (int i=1;i<A.length-1;i++) {
+            s1[i] = s1[i-1] + A[i];
         }
-        return sum;
+
+        int [] s2 = new int[A.length-1];
+        s2[A.length-2] = A[A.length-1];
+        for (int i=A.length-3;i>=0;i--) {
+            s2[i] = s2[i+1] + A[i+1];
+        }
+
+        int finalSum = Integer.MAX_VALUE;
+        for (int j=0;j<s1.length;j++) {
+            int sum = Math.abs(s1[j]-s2[j]);
+            if (sum < finalSum)
+                finalSum = sum;
+        }
+        return finalSum;
     }
 }
