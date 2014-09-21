d1f1e42948b70770e36357db3cda41069d7212eb
Aysar Khalid <aysar.khalid@wash.ly>
2014-09-20 21:46:10 -0400

    Caterpillar iteration alg

---
+++ FB/src/Caterpillar.java
@@ -0,0 +1,20 @@
+
+public class Caterpillar {
+
+  public int find_s(int[] A, int s) {
+    int front = 0;
+    int total = 0;
+    
+    for (int back= 0; back<A.length; back++) {
+      while ((front < A.length) && (total + A[front] <= s)) {
+        total += A[front];
+        front++;
+      }
+      if (total == s)
+        return true
+      total -= A[back];
+    }
+    return false;
+  }
+
+}
