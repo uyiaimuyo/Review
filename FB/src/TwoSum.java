d5b74f5e6045be8311459d31196f1650d5a7e971
Aysar Khalid <aysar.khalid@wash.ly>
2014-09-22 22:30:01 -0400

    Create TwoSum.java

---
+++ FB/src/TwoSum.java
@@ -0,0 +1,19 @@
+public class TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        
+        HashMap<Integer,Integer> num_to_index = new HashMap<Integer,Integer>();
+        for (int i=0; i<numbers.length; i++) {
+            num_to_index.put(numbers[i],i);    
+        }
+        int[] r = new int[2];
+        for (int i=0; i<numbers.length; i++) {
+            if (num_to_index.containsKey(target - numbers[i])) {
+                r[0] = i+1;
+                r[1] = num_to_index.get(target - numbers[i]) + 1;
+                if (num_to_index.get(target - numbers[i]) != i)
+                    break;
+            }
+        }
+        return r;
+    }
+}
